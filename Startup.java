fbc25b33d064f2b51051b8aea05b691d5d2a0b8c
sowmie <sowmya.ramani@gmail.com>
2019-04-11 19:18:53 -0700

    Create Startup.java

---
+++ Startup.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+
+public class Startup{
+
+public void printme(){
+   System.out.println("Starting up...");
+}
+   
+public static void main(String[] args0){
+      printme();
+}

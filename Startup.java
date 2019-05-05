8e277c69485acd99397cc3fb367a8b296db3ff47
emrysari <vinodsowmie@gmail.com>
2019-05-04 22:50:59 -0700

    First committo forked feature branch.wq :q!d^Oxx xxX# Please enter the commit message for your changes. Lines starting

--- Startup.java
+++ Startup.java
@@ -3,10 +3,17 @@ import java.lang.String;
 public class Startup{
 
 public void printme(){
+
    System.out.println("Starting up...");
    System.out.printn("Change me up..");
+   System.out.println("Changing it the second time..");
+
 }
-   
+
 public static void main(String[] args0){
+
+
       printme();
+
+           
 }
